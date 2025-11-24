02002d3f12ba39726f18ec7c720bc97cf98fd076
JeremyLrs <hello@laurensjeremy.com>
2025-11-24 10:21:52 +0100

    Create function floor which takes a float n as argument and returns the floor of the float.

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+floor of a float
+"""
+from math import floor as math_floor
+
+
+def floor(n: float) -> int:
+    """
+    floor of a float
+    """
+    return math_floor(n)
