0220e1345e55d0857b24f8c80ce45eb5972c3d02
JeremyLrs <hello@laurensjeremy.com>
2025-12-01 14:57:15 +0100

    Chmod file

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,4 +8,8 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """
     Calculate the start and the end of pagination
     """
-    return (page_size * (page - 1), page_size * page)
+
+    page_init = (page - 1) * page_size
+    end_page = page_init + page_size
+
+    return (page_init, end_page)
