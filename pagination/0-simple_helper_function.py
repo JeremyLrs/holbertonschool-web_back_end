d31c1eb23baed7f58f446fc38557b147eff6f5fb
JeremyLrs <hello@laurensjeremy.com>
2025-12-01 14:06:20 +0100

    Add whitespace around operator

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,7 +9,7 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     Calculate the start and the end of pagination
     """
 
-    page_init = (page -1) * page_size
+    page_init = (page - 1) * page_size
     end_page = page_init + page_size
 
     return (page_init, end_page)
