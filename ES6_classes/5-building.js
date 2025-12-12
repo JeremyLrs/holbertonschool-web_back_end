95b511d2f3c4b0188cfb4337f21ca22980834ad8
JeremyLrs <hello@laurensjeremy.com>
2025-12-12 14:52:29 +0100

    Add abstract Building with sqft getter and mandatory evacuationMsg

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== "number") {
+      throw TypeError("Sqft musft be a number");
+    }
+
+    if (this.constructor.name !== "Building" && typeof this.evacuationWarningMessage !== "function") {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+
+    this._sqft = sqft;
+  }
+
+    get sqft() {
+      return this._sqft;
+    }
+}
